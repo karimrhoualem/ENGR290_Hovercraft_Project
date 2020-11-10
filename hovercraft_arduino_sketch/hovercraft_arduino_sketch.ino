0d15ffa2d7d4837cb75f1825d9963b814cfac3aa
Karim <karim.rhoualem@gmail.com>
2020-11-10 01:09:26 -0500

    Added logic to move the hovercraft after receiving sensor values. #1

--- hovercraft_arduino_sketch/hovercraft_arduino_sketch.ino
+++ hovercraft_arduino_sketch/hovercraft_arduino_sketch.ino
@@ -1,3 +1,14 @@
+enum Fan {
+  OFF,
+  ON
+};
+
+enum Throttle {
+  THROTTLE_OFF,
+  THROTTLE_MEDIUM,
+  THROTTLE_HIGH
+};
+
 enum Direction {
   FRONT,
   LEFT,
@@ -24,13 +35,54 @@ Direction compareSensorDistances(float frontSensorValue, float leftSensorValue, 
   }
 };
 
+void Move(Direction maxDirection, Throttle leftThrottle, Throttle rightThrottle, Fan leftFan, Fan rightFan) {
+  if (maxDirection == LEFT) {
+    leftThrottle = THROTTLE_OFF;
+    rightThrottle = THROTTLE_MEDIUM;
+    leftFan = OFF;
+    rightFan = ON;
+  }
+  else if (maxDirection == RIGHT) {
+    leftThrottle = THROTTLE_MEDIUM;
+    rightThrottle = THROTTLE_OFF;
+    leftFan = ON;
+    rightFan = OFF;
+  }
+  else if (maxDirection == FRONT) {
+    leftThrottle = THROTTLE_HIGH;
+    rightThrottle = THROTTLE_HIGH;
+    leftFan = ON;
+    rightFan = ON;
+  }
+  else if (maxDirection == ERR) {
+    //Handle error
+  }
+}
+
+/***********************************************************************
+*                            GLOBAL VARIABLES                          *
+************************************************************************/
+//Define sensor variables
+float frontSensorValue = 0;
+float leftSensorValue = 0;
+float rightSensorValue = 0;
+
+//Define direction variable
+Direction hovercraftDirection;
+
+//Define throttle and fan variables
+//Is it redundant to set throttle AND fans to ON/OFF??
+Throttle leftThrottle = THROTTLE_OFF;
+Throttle rightThrottle = THROTTLE_OFF;
+Fan leftFan = OFF;
+Fan rightFan = OFF;
 
 void setup() {
-  // put your setup code here, to run once:
-  
+
 }
 
 void loop() {  
-  
-  
+  hovercraftDirection = compareSensorDistances(frontSensorValue, leftSensorValue, rightSensorValue);
+
+  Move(hovercraftDirection, leftThrottle, rightThrottle, leftFan, rightFan);
 }
