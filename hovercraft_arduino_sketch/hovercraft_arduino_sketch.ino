42a25807e527183372c27fc5117bfb6fe773fa60
Karim <karim.rhoualem@gmail.com>
2020-11-11 02:35:06 -0500

    Improved the proximity sensity algorithm. #1

--- hovercraft_arduino_sketch/hovercraft_arduino_sketch.ino
+++ hovercraft_arduino_sketch/hovercraft_arduino_sketch.ino
@@ -4,15 +4,15 @@
 
 // Enum that specifies the current fan setting.
 enum Fan {
-  OFF,
-  ON
+  OFF = 0,
+  ON = 1
 };
 
 // Enum that specifies the current throttle setting for a fan.
 enum Throttle {
-  THROTTLE_OFF,
-  THROTTLE_MEDIUM,
-  THROTTLE_HIGH
+  THROTTLE_OFF = 0,
+  THROTTLE_MEDIUM = 1,
+  THROTTLE_HIGH = 2
 };
 
 // Enum that specifies the direction the hovercraft will move toward.
@@ -27,19 +27,21 @@ enum Direction {
 *                            GLOBAL VARIABLES                          *
 ************************************************************************/
 //Initialize global sensor variables
-float frontSensorValue = 0;
-float leftSensorValue = 0;
-float rightSensorValue = 0;
+float frontSensorValue;
+float leftSensorValue;
+float rightSensorValue;
 
 //Initialize global direction variable
 Direction hovercraftDirection;
 
 //Initialize global throttle and fan variables
-//Is it redundant to set throttle AND fans to ON/OFF??
-Throttle leftThrottle = THROTTLE_OFF;
-Throttle rightThrottle = THROTTLE_OFF;
-Fan leftFan = OFF;
-Fan rightFan = OFF;
+Throttle leftFanThrottle;;
+Throttle rightFanThrottle;
+float leftConvertedThrottle;
+float rightConvertedThrottle;
+
+Fan leftFanState = ON;
+Fan rightFanState = ON;
 
 /***********************************************************************
 *                                FUNCTIONS                             *
@@ -47,9 +49,9 @@ Fan rightFan = OFF;
 
 // Takes the sensor values as parameters, finds the max, and then positions the hovercraft towards the max direction.
 Direction compareSensorDistances(float frontSensorValue, float leftSensorValue, float rightSensorValue) {
-
-  float intermediaryMax = max(leftSensorValue, rightSensorValue);
-  float maxSensorValue = max(intermediaryMax, frontSensorValue);
+  
+  float maxSensorValue = max(leftSensorValue, rightSensorValue);
+  maxSensorValue = max(maxSensorValue, frontSensorValue);
 
   if (maxSensorValue == frontSensorValue) {
     return Direction::FRONT;
@@ -69,42 +71,99 @@ Direction compareSensorDistances(float frontSensorValue, float leftSensorValue, 
  * Takes the direction to move the hovercraft in, the fan throttle settings, and the fan statuses as parameters, 
  * and then sets the throttle and fan settings accordingly.
  */
-void Move(Direction maxDirection, Fan leftFan, Fan rightFan) {
+void Move(Direction maxDirection, Fan leftFanState, Fan rightFanState) {
   if (maxDirection == LEFT) {
-    leftThrottle = THROTTLE_OFF;
-    rightThrottle = THROTTLE_MEDIUM;
-    leftFan = OFF;
-    rightFan = ON;
+    leftFanThrottle = THROTTLE_OFF;
+    rightFanThrottle = THROTTLE_HIGH;
+    leftFanState = OFF;
+    rightFanState = ON;
   }
   else if (maxDirection == RIGHT) {
-    leftThrottle = THROTTLE_MEDIUM;
-    rightThrottle = THROTTLE_OFF;
-    leftFan = ON;
-    rightFan = OFF;
+    leftFanThrottle = THROTTLE_HIGH;
+    rightFanThrottle = THROTTLE_OFF;
+    leftFanState = ON;
+    rightFanState = OFF;
   }
   else if (maxDirection == FRONT) {
-    leftThrottle = THROTTLE_HIGH;
-    rightThrottle = THROTTLE_HIGH;
-    leftFan = ON;
-    rightFan = ON;
+    leftFanThrottle = THROTTLE_HIGH;
+    rightFanThrottle = THROTTLE_HIGH;
+    leftFanState = ON;
+    rightFanState = ON;
   }
   else if (maxDirection == ERR) {
     //Handle error
   }
 }
 
+void ConvertThrottle(Throttle leftThrottle, Throttle rightThrottle) {
+    switch (leftThrottle) {
+      case THROTTLE_OFF:
+        leftConvertedThrottle = 0;
+        break;
+      case THROTTLE_MEDIUM:
+        leftConvertedThrottle = 0.5;
+        break;
+      case THROTTLE_HIGH:
+        leftConvertedThrottle = 1;
+        break;
+    }
+
+    switch (rightThrottle) {
+      case THROTTLE_OFF:
+        rightConvertedThrottle = 0;
+        break;
+      case THROTTLE_MEDIUM:
+        rightConvertedThrottle = 0.5;
+        break;
+      case THROTTLE_HIGH:
+        rightConvertedThrottle = 1;
+        break;
+    }
+}
 
 /***********************************************************************
 *                               MAIN CODE                              *
 ************************************************************************/
 // Main setup function
 void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200); 
+  Serial.setTimeout(5);
+}
 
+void serialWait(){
+  while(Serial.peek() != 's') {
+    char t = Serial.read();
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1);                      
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(1);
+  }
+  char t = Serial.read(); //discard the s
 }
 
 // Main loop function
-void loop() {  
+void loop() { 
+  //Read serial port until sensor data is sent
+  serialWait();
+  
+  frontSensorValue = Serial.parseFloat();
+  leftSensorValue = Serial.parseFloat();
+  rightSensorValue = Serial.parseFloat();
+  
   hovercraftDirection = compareSensorDistances(frontSensorValue, leftSensorValue, rightSensorValue);
 
-  Move(hovercraftDirection, leftFan, rightFan);
+  Move(hovercraftDirection, leftFanState, rightFanState);
+
+  ConvertThrottle(leftFanThrottle, rightFanThrottle);
+  
+  Serial.print(leftFanState);
+  Serial.write(",");
+  Serial.print(rightFanState);
+  Serial.write(",");
+  Serial.print(leftConvertedThrottle);
+  Serial.write(",");
+  Serial.print(rightConvertedThrottle);
+  Serial.write("\r\n");
 }
